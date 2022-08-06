37a400a803dfb7cf52d15229a8763229c1bddb11
Akshat Lambhate <akshatlambhate180302@gmail.com>
2022-08-06 14:04:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let screen = document.getElementById("show")
+console.log(screen) 
+let count =0
+function increment(){
+  // console.log("button clicked")
+
+    count += 1
+    show.innerText = count
+}
